753ce2c631101d20312d90e4c1590e659598d7bb
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-08-27 15:47:58 +0200

    Refactor and add to options.lua

--- lua/mjh/options.lua
+++ lua/mjh/options.lua
@@ -1,34 +1,41 @@
-vim.cmd("autocmd!")
+local options = {
+	number = true,
+	cursorline = true,
+	signcolumn = 'yes',
+	ttimeoutlen = 10,
+	numberwidth = 4,
+	mouse = 'a',
+	title = true,
+	autoindent = true,
+	smartindent = true,
+	hlsearch = true,
+	incsearch = true,
+	ignorecase = true,
+	smartcase = true,
+	wrapscan = true,
+	backup = false,
+	showcmd = false,
+	cmdheight = 1,
+	laststatus = 2,
+	expandtab = true,
+	shell = 'zsh',
+	smarttab = true,
+	breakindent = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	wrap = false,
+	termguicolors = true,
+  clipboard = 'unnamedplus',
+  conceallevel = 0,
+  pumheight = 10,
+  showmode = false,
+  splitbelow = true,
+  splitright = true,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  whichwrap = 'b,s,<,>,[,]',
+}
 
-vim.wo.number = true
-vim.opt.ttimeoutlen = 0
-vim.opt.numberwidth = 3
-vim.wo.cursorline = true -- Enable highlighting of the current line
-vim.wo.signcolumn = 'yes' -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.mouse = 'a'
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.wrapscan = true
-vim.opt.backup = false
-vim.opt.showcmd = false
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = 'zsh'
-vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.wrap = false -- No Wrap lines
-vim.opt.backspace = { 'start', 'eol', 'indent' }
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' }
-vim.opt.termguicolors = true
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
