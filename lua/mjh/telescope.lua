5cb8b2368ec6ad8777d9171344c02f95b9fff268
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-08-28 16:38:03 +0200

    Add telescope config

---
+++ lua/mjh/telescope.lua
@@ -0,0 +1,15 @@
+local status, telescope = pcall(require, 'telescope')
+if not status then return end
+
+telescope.setup({
+  pickers = {
+    find_files = {
+      theme = 'dropdown',
+      previewer = false,
+    },
+    buffers = {
+      theme = 'dropdown',
+      previewer = false,
+    }
+  }
+})
