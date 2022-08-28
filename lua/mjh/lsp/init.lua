67fec9457d119607a8c4010d1b925be13204fe3d
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-08-28 14:21:23 +0200

    Add comments and extract diagnostics options to seperate file

--- lua/mjh/lsp/init.lua
+++ lua/mjh/lsp/init.lua
@@ -7,12 +7,16 @@ if not status_masonlsp then return end
 local status_lsp, lsp = pcall(require, 'lspconfig')
 if not status_lsp then return end
 
+-- Create default server options table
 local default_server_options = {
   on_attach = require('mjh.lsp.on_attach'),
   flags = { debounce_text_changes = 150 },
   capabilities = require('mjh.lsp.capabilities'),
 }
 
+-- Setup mason and mason-lspconfig
+-- Default handlers for each installed server
+-- and override handlers for servers with custom options
 mason.setup()
 mason_lsp.setup()
 mason_lsp.setup_handlers({
@@ -28,33 +32,5 @@ mason_lsp.setup_handlers({
   end
 })
 
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-
-for  _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-local config = {
-  virtual_text = false,
-  signs = {
-    active = signs,
-  },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
-}
-
-vim.diagnostic.config(config)
+-- Configure vim diagnostics
+vim.diagnostic.config(require('mjh.lsp.settings.diagnostics'))
