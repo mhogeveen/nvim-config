207aaac1d2aaa1e9d178699b6b6dfcae4d7f259c
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-08-28 14:25:42 +0200

    Rename options to configs

--- lua/mjh/lsp/init.lua
+++ lua/mjh/lsp/init.lua
@@ -7,8 +7,8 @@ if not status_masonlsp then return end
 local status_lsp, lsp = pcall(require, 'lspconfig')
 if not status_lsp then return end
 
--- Create default server options table
-local default_server_options = {
+-- Create default server config table
+local default_server_config = {
   on_attach = require('mjh.lsp.on_attach'),
   flags = { debounce_text_changes = 150 },
   capabilities = require('mjh.lsp.capabilities'),
@@ -16,21 +16,21 @@ local default_server_options = {
 
 -- Setup mason and mason-lspconfig
 -- Default handlers for each installed server
--- and override handlers for servers with custom options
+-- and override handlers for servers with custom config
 mason.setup()
 mason_lsp.setup()
 mason_lsp.setup_handlers({
   -- Default server config
   function (server_name)
-    lsp[server_name].setup(default_server_options)
+    lsp[server_name].setup(default_server_config)
   end,
 
   -- Lua config
   ['sumneko_lua'] = function()
-    local lua_server_options = require('mjh.lsp.settings.lua')
-    lsp['sumneko_lua'].setup(vim.tbl_deep_extend('force', lua_server_options, default_server_options))
+    local lua_server_config = require('mjh.lsp.configs.lua')
+    lsp['sumneko_lua'].setup(vim.tbl_deep_extend('force', lua_server_config, default_server_config))
   end
 })
 
 -- Configure vim diagnostics
-vim.diagnostic.config(require('mjh.lsp.settings.diagnostics'))
+vim.diagnostic.config(require('mjh.lsp.configs.diagnostics'))
