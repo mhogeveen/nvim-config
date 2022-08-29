825de521c579ae584980eb592412a83c6e5165e4
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-08-29 10:50:55 +0200

    Change nvim colorscheme

--- lua/mjh/colortheme.lua
+++ lua/mjh/colortheme.lua
@@ -1,9 +1,34 @@
-local status, github_theme = pcall(require, 'github-theme')
+--local theme_name = 'github_config'
+local theme_name = 'kanagawa'
+
+local status, theme = pcall(require, theme_name)
 if not status then return end
 
-github_theme.setup({
+local github_config = {
 	theme_style = "dark_default",
 	transparent = false,
 	function_style = "italic",
 	variable_style = "italic",
-})
+}
+
+local kanagawa_config = {
+  undercurl = true,                           -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = { italic = true },
+  keywordStyle = { italic = false },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  variablebuiltinStyle = { italic = true},
+  specialReturn = true,                       -- special highlight for the return keyword
+  specialException = true,                    -- special highlight for exception handling keywords
+  transparent = false,                        -- do not set background color
+  dimInactive = false,                        -- dim inactive window `:h hl-NormalNC`
+  globalStatus = true,                        -- adjust window separators highlight for laststatus=3
+  terminalColors = true,                      -- define vim.g.terminal_color_{0,17}
+  colors = {},
+  overrides = {},
+  theme = "default"                           -- 'default' or 'light' (experimental)
+}
+
+theme.setup(kanagawa_config)
+vim.cmd("colorscheme kanagawa")
