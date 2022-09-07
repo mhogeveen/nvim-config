1004dfd194b2e23a1e588309ea19851be6bcf453
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-07 13:51:06 +0200

    Update null-ls config so formatting works

--- lua/mjh/null-ls.lua
+++ lua/mjh/null-ls.lua
@@ -1,26 +1,70 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+  return
 end
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name ~= "tsserver"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
-	sources = {
-		--null_ls.builtins.code_actions.eslint_d,
-		--null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.formatting.eslint_d,
-		--null_ls.builtins.diagnostics.editorconfig_checker,
+  sources = {
+    --null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({
+          ".eslintrc",
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+          ".eslintrc.yaml",
+          ".eslintrc.yml",
+          ".eslintrc.json",
+        })
+      end,
+    }),
+    null_ls.builtins.formatting.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({
+          ".eslintrc",
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+          ".eslintrc.yaml",
+          ".eslintrc.yml",
+          ".eslintrc.json",
+        })
+      end,
+    }),
+    --null_ls.builtins.diagnostics.editorconfig_checker,
     --null_ls.builtins.completion.luasnip,
-		--null_ls.builtins.diagnostics.luacheck,
-		null_ls.builtins.formatting.stylua,
-		--null_ls.builtins.diagnostics.php,
-		--null_ls.builtins.diagnostics.phpcs,
-		null_ls.builtins.formatting.phpcsfixer,
-		--null_ls.builtins.diagnostics.stylelint,
-		null_ls.builtins.formatting.stylelint,
-		--null_ls.builtins.diagnostics.tsc,
-		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.trim_newlines,
-		null_ls.builtins.formatting.trim_whitespace,
-	},
-  on_attach = require("mjh.lsp.on_attach")
+    --null_ls.builtins.diagnostics.luacheck,
+    null_ls.builtins.formatting.stylua,
+    --null_ls.builtins.diagnostics.php,
+    --null_ls.builtins.diagnostics.phpcs,
+    null_ls.builtins.formatting.phpcsfixer,
+    --null_ls.builtins.diagnostics.stylelint,
+    null_ls.builtins.formatting.stylelint,
+    --null_ls.builtins.diagnostics.tsc,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.trim_newlines,
+    null_ls.builtins.formatting.trim_whitespace,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end,
 })
