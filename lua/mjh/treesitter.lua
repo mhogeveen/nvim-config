41b0aa8cc20bd40d55140402c7a6097f589e34ef
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-07 13:51:37 +0200

    Set option so php syntax highlighting works correctly

--- lua/mjh/treesitter.lua
+++ lua/mjh/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 treesitter.setup({
   auto_install = true,
-  highlight = { enable = true },
+  highlight = { enable = true, additional_vim_regex_highlighting = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true },
   indent = { enable = true },
