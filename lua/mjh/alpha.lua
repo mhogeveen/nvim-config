50000ff4a7055ac8d9deaeb6a13ec6ef925d94d4
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-11 19:51:27 +0200

    Modify dashboard buttons to actually cd into directories

--- lua/mjh/alpha.lua
+++ lua/mjh/alpha.lua
@@ -1,28 +1,30 @@
-local status, alpha = pcall(require, 'alpha')
-if not status then return end
+local status, alpha = pcall(require, "alpha")
+if not status then
+  return
+end
 
-local ascii = require('mjh.utils.ascii')
-local dashboard = require('alpha.themes.dashboard')
+local ascii = require("mjh.utils.ascii")
+local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = ascii.alien
 
 dashboard.section.buttons.val = {
-    dashboard.button( "f",  "  - Find file",     ":Telescope find_files<CR>" ),
-    dashboard.button( "r",  "  - Recent",        ":Telescope oldfiles<CR>"   ),
-    dashboard.button( "n",  "  - Nvim Config",   ":e ~/.config/nvim<CR>"     ),
-    dashboard.button( "t",  "  - Kitty Config",  ":e ~/.config/kitty<CR>"    ),
-    dashboard.button( "q",  "  - Quit",          ":qa<CR>"                   ),
+  dashboard.button("f", "  - Find file", ":Telescope find_files<CR>"),
+  dashboard.button("r", "  - Recent", ":Telescope oldfiles<CR>"),
+  dashboard.button("n", "  - Nvim Config", ":cd ~/.config/nvim | e .<CR>"),
+  dashboard.button("t", "  - Kitty Config", ":cd ~/.config/kitty | e .<CR>"),
+  dashboard.button("q", "  - Quit", ":qa<CR>"),
 }
 dashboard.section.buttons.opts.spacing = 0
 
-dashboard.section.footer.val = require('alpha.fortune')
+dashboard.section.footer.val = require("alpha.fortune")
 dashboard.config.layout = {
-  { type = 'padding', val = 8 },
+  { type = "padding", val = 8 },
   dashboard.section.header,
-  { type = 'padding', val = 4 },
+  { type = "padding", val = 4 },
   dashboard.section.buttons,
-  { type = 'padding', val = 4 },
-  dashboard.section.footer
+  { type = "padding", val = 4 },
+  dashboard.section.footer,
 }
 alpha.setup(dashboard.opts)
 
