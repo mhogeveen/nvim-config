2eec9b7200d99b26f78f7157d72976816b005390
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-11 19:41:08 +0200

    Add protected call && formatting

--- lua/mjh/lualine.lua
+++ lua/mjh/lualine.lua
@@ -1,9 +1,14 @@
-require('lualine').setup({
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'auto', -- auto, catppuccin
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    theme = "catppuccin", -- auto, catppuccin
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -15,42 +20,42 @@ require('lualine').setup({
       statusline = 1000,
       tabline = 1000,
       winbar = 1000,
-    }
+    },
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = { "mode" },
     lualine_b = {
       {
-        'filename',
+        "filename",
         file_status = true,
         symbols = {
-          modified = ' ⦋+⦌',
-          readonly = ' ⦋-⦌',
-          unnamed = '⦋*⦌',
-        }
-      }
+          modified = " [+]",
+          readonly = " [-]",
+          unnamed = "[*]",
+        },
+      },
     },
-    lualine_c = {'branch', 'diff'},
-    lualine_x = {'encoding'},
+    lualine_c = { "branch", "diff" },
+    lualine_x = { "encoding" },
     lualine_y = {
-      'filesize',
+      "filesize",
       {
-        'filetype',
+        "filetype",
         colored = false,
-      }
+      },
     },
-    lualine_z = {'location'}
+    lualine_z = { "location" },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = {}
+  extensions = {},
 })
