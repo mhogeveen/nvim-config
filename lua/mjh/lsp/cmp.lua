4b86253c79c7786611a82508ea6f33cf784a6aca
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-13 17:24:56 +0200

    Polish cmp config

--- lua/mjh/lsp/cmp.lua
+++ lua/mjh/lsp/cmp.lua
@@ -45,11 +45,11 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-2), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(2), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    --["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
@@ -65,10 +65,13 @@ cmp.setup({
   }),
   window = {
     completion = {
-      col_offset = -3,
+      col_offset = 3,
       side_padding = 0,
+      border = nil,
     },
-    documentation = cmp.config.window.bordered({ "╭", "─", "╮", "│", "╯", "─", "╰", "│" }),
+    documentation = {
+      border = nil,
+    },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
